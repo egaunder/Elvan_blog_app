60b2f02d8f98be12504f4bead7e25de293307692
egaunder <egaunder@gmail.com>
2016-06-13 15:35:02 -0700

    created a relationship between articles and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ActiveRecord::Base 
+  has_many :articles
+
+  # will lowercase the email before saving
+  before_save { self.email = email.downcase }
+
   validates :username, presence: true, 
             length: { minimum: 3, maximum: 25 },
             uniqueness: { case_sensitive: false }
-
- 
+  
+  # pass in valid regex to check if email is valid
   VALID_EMAIL_REGEX= /\A[w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
             uniqueness: { case_sensitive: false },
