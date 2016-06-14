052c52ec97c82d607f3f531494a0755892c32c4d
egaunder <egaunder@gmail.com>
2016-06-14 05:52:50 -0700

    added partial for edit_user and new_user

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'about', to: 'pages#about'
+  
   get 'signup', to: 'users#new'
-  post 'users', to: 'users#create'
+  resources :users, except: [:new]
 
   resources :articles
 
