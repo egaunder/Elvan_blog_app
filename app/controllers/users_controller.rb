ce33cdcbe5b88388b7aaf318ba10e335ef3b7686
egaunder <egaunder@gmail.com>
2016-06-20 12:12:24 -0700

    made changes to users_controller for debugging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Tech Blog #{@user.username}"
       redirect_to articles_path
